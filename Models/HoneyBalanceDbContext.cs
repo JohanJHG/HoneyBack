6d8465e8fceee43984bd30464602c26f9364e1ea
Johan Hernandez <jhoan.rhz@gmail.com>
2025-11-30 18:24:01 -0500

    fix error

--- Models/HoneyBalanceDbContext.cs
+++ Models/HoneyBalanceDbContext.cs
@@ -34,8 +34,15 @@ public partial class HoneyBalanceDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=JOHAN\\SQLEXPRESS;Database=HoneyBalanceDB;Integrated Security=true;TrustServerCertificate=True");
+    {
+        // Solo configurar si no hay opciones previas (para design-time/migraciones)
+        // En runtime, Program.cs y docker-compose.yml configuran la conexión
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Conexión local para desarrollo/migraciones
+            optionsBuilder.UseSqlServer("Server=JOHAN\\SQLEXPRESS;Database=HoneyBalanceDB;Integrated Security=true;TrustServerCertificate=True");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
